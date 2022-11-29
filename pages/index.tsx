0f5ad5fa54ff1fd344a69b7315f606bc73d7489a
Tam Saunders <tambo@users.noreply.github.com>
2022-11-29 18:20:32 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Tambo's <a href="https://nextjs.org">Next.js!</a> Site :)
+          Welcome to Tambo&apos;s <a href="https://nextjs.org">Next.js!</a> Site :)
         </h1>
 
         <p className={styles.description}>
